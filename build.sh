5a14ebffd5e5ca38e325b18243f378a332875d20
root <root@ip-172-31-80-115.ec2.internal>
2026-03-27 09:07:51 +0000

    updated scrpting file

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Login to DockerHub
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+# Stop & remove old container
 docker stop python-cals || true
 docker rm python-cals || true
 
+# Build image
 docker build -t calculator-app .
 
+# Run container
 docker run -d --name python-cals -p 8501:8501 calculator-app
 
-docker tag react-app rahul9786/calculator-py:latest
+# Tag image (FIXED)
+docker tag calculator-app rahul9786/calculator-py:latest
 
+# Push image
 docker push rahul9786/calculator-py:latest
